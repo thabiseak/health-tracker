607627152188f3bba3cfe80ffb3e6bf5f8870b32
Thavanesan Thabiseak <thabuthavanesan@gmail.com>
2024-12-17 12:34:00 +0530

    Add ImageConstant class for centralized image asset management

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,70 @@
+ //ignore_for_file: must be immutable
+class ImageConstant {
+  //Image folder path
+  static String imagePath = 'assets/images/';
+
+  //Welcome Screen images
+  static String img8bbe484b1091ae9 = '$imagePath/img_8bbe484b1091ae9.png';
+
+  //Login images
+  static String imgFacebook= '$imagePath/imgFacebook.svg';
+
+  static String imgclock = '$imagePath/img_clock.svg';
+
+  static String imgTelevision = '$imagePath/img_television.svg';
+
+  static String imgEllipse = '$imagePath/img_ellipse_4.png';
+
+// SignUp images
+  static String imgEllipse5 = '$imagePath/img_ellipse_5.png'; 
+  
+  static String imgEyeoffOnerrorcontainer = 
+      '$imagePath/img_eyeoff_onerrorcontainer.svg';
+
+  static String imgEllipse4188x430 = '$imagePath/img_ellipse_4_188x430.png';
+
+//About images
+  static String imgGroup24 = '$imagePath/img_group_24.png';
+
+  static String imgGroup166 = '$imagePath/img_group_166.png';
+
+// Daily plan images
+static String imgDailyPlan = '$imagePath/img_daily_plan.png';
+
+static String imgGroup146 = '$imagePath/img_group_146.png';
+
+// Progress images
+static String img71 = '$imagePath/img_7_1.png';
+
+static String img5RemovebgPreview = '$imagePath/img_5_removebg_preview.png';
+
+// Profile images
+static String imgGroup41 = '$imagePath/img_group_41.png';
+
+static String img8 = '$imagePath/img_8.png';
+
+static String imgUser = '$imagePath/img_user.svg';
+
+static String imgArrowLeft = '$imagePath/img_arrow_left.svg';
+
+static String imgHome = '$imagePath/img_home.svg';
+
+static String imgHeartRatePulse = '$imagePath/img_heart_rate_pulse.svg';
+
+static String imgTelevisionBlack900 = '$imagePath/img_television_black_900.svg';
+
+// Common images
+static String imgGroup53 = '$imagePath/img_group_53.png';
+
+static String imgImage = '$imagePath/img_image.png';
+
+static String imgEyeOff = '$imagePath/img_eye_off.svg';
+
+static String imgEllipse2 = '$imagePath/img_ellipse_2.svg';
+
+static String imgArrowLeftGray50 = '$imagePath/img_arrow_left_gray_50.svg';
+
+static String imgImage100x100 = '$imagePath/img_image_100x100.png';
+
+static String imageNotFound = 'assets/images/image_not_found.png';
+}
