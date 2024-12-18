0d2701c3e36151610ab0ef63709be8217935eeb5
Thavanesan Thabiseak <thabuthavanesan@gmail.com>
2024-12-18 14:35:50 +0530

    Add AboutState class to manage state for AboutScreen with input controllers

---
+++ lib/presentation/about_screen/bloc/about_state.dart
@@ -0,0 +1,56 @@
+part of 'about_bloc.dart';
+
+/// Represents the state of About in the application.
+
+// ignore_for_file: must_be_immutable
+class AboutState extends Equatable {
+  AboutState({
+    this.genderInputController,
+    this.ageInputController,
+    this.heightInputController,
+    this.weightInputController,
+    this.radioGroup = "",
+    this.aboutModelObj,
+  });
+
+  TextEditingController? genderInputController;
+
+  TextEditingController? ageInputController;
+
+  TextEditingController? heightInputController;
+
+  TextEditingController? weightInputController;
+
+  AboutModel? aboutModelObj;
+  
+  String radioGroup;
+
+  @override
+  List<Object?> get props => [
+        genderInputController,
+        ageInputController,
+        heightInputController,
+        weightInputController,
+        radioGroup,
+        aboutModelObj,
+      ];
+
+  AboutState copyWith({
+    TextEditingController? genderInputController,
+    TextEditingController? ageInputController,
+    TextEditingController? heightInputController,
+    TextEditingController? weightInputController,
+    String? radioGroup,
+    AboutModel? aboutModelObj,
+  }) {
+    return AboutState(
+      genderInputController:
+          genderInputController ?? this.genderInputController,
+      ageInputController: ageInputController ?? this.ageInputController,
+      heightInputController: heightInputController ?? this.heightInputController,
+      weightInputController: weightInputController ?? this.weightInputController,
+      radioGroup: radioGroup ?? this.radioGroup,
+      aboutModelObj: aboutModelObj ?? this.aboutModelObj,
+    );
+  }
+}
