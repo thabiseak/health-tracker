df23a264a16e5769182add29608584703023595a
Thavanesan Thabiseak <thabuthavanesan@gmail.com>
2024-12-18 14:48:19 +0530

    Refactor WelcomeScreen to improve code readability and add _buildIntroSection method for better structure

--- lib/presentation/welcome_screen/welcome_screen.dart
+++ lib/presentation/welcome_screen/welcome_screen.dart
@@ -6,52 +6,90 @@ import 'bloc/welcome_bloc.dart';
 import 'models/welcome_model.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({Key? key})
-      : super(
-          key: key,
-        );
+  const WelcomeScreen({Key? key}) : super(key: key);
 
   static Widget builder(BuildContext context) {
     return BlocProvider<WelcomeBloc>(
-      create: (context) => WelcomeBloc(WelcomeState(
-        welcomeModelObj: WelcomeModel(),
-      ))
+      create: (context) => WelcomeBloc(WelcomeState(welcomeModelObj: WelcomeModel()))
         ..add(WelcomeInitialEvent()),
-      child: WelcomeScreen(),
+      child: const WelcomeScreen(),
     );
   }
 
   @override
-Widget build(BuildContext context) {
-  return BlocBuilder<WelcomeBloc, WelcomeState>(
-    builder: (context, state) {
-      return SafeArea(
-        child: Scaffold(
-          body: SizedBox(
-            width: double.maxFinite,
-            child: SingleChildScrollView(
-              child: Container(
-                height: SizeUtils.height,
-                width: double.maxFinite,
-                padding: EdgeInsets.only(top: 8.h),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    CustomImageView(
-                      imagePath: ImageConstant.img8bbe484b1091ae9,
-                      height: 610.h,
-                      width: double.maxFinite,
-                    ),
-                    SizedBox(height: 34.h),
-                    _buildIntroSection(context)
-                  ],
+  Widget build(BuildContext context) {
+    return BlocBuilder<WelcomeBloc, WelcomeState>(
+      builder: (context, state) {
+        return SafeArea(
+          child: Scaffold(
+            body: SizedBox(
+              width: double.maxFinite,
+              child: SingleChildScrollView(
+                child: Container(
+                  height: SizeUtils.height,
+                  width: double.maxFinite,
+                  padding: EdgeInsets.only(top: 8.h),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      CustomImageView(
+                        imagePath: ImageConstant.img8bbe484b1091ae9,
+                        height: 610.h,
+                        width: double.maxFinite,
+                      ),
+                      SizedBox(height: 34.h),
+                      _buildIntroSection(context),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      );
-    },
-  );
+        );
+      },
+    );
+  }
+
+  /// Section Widget
+  Widget _buildIntroSection(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      margin: EdgeInsets.symmetric(horizontal: 24.h),
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.maxFinite,
+            child: Column(
+              children: [
+                Text(
+                  "msg_workout_for_health".tr,
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+                SizedBox(height: 18.h),
+                Text(
+                  "msg_your_journey_to".tr,
+                  style: CustomTextStyles.bodyLargeRobotoOnErrorContainer,
+                ),
+                SizedBox(height: 58.h),
+                CustomElevatedButton(
+                  height: 50.h,
+                  text: "lbl_get_started".tr,
+                  margin: EdgeInsets.only(left: 44.h, right: 42.h),
+                  buttonTextStyle: CustomTextStyles.headlineSmallOnErrorContainer,
+                  onPressed: () => onTapGetStarted(context),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// Navigates to the loginScreen when the action is triggered.
+  void onTapGetStarted(BuildContext context) {
+    NavigatorService.pushNamed(AppRoutes.loginScreen);
+  }
 }
+
 
