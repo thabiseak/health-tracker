b0c323cf99f565e36126770faa90e868b37777b5
Thavanesan Thabiseak <thabuthavanesan@gmail.com>
2024-12-18 14:53:18 +0530

    Add WelcomeModel class for the Welcome screen with Equatable support

---
+++ lib/presentation/welcome_screen/models/welcome_model.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+/// This class represents the model used in the Welcome screen.
+/// It extends [Equatable] for efficient comparison of instances.
+class WelcomeModel extends Equatable {
+
+  /// Creates a copy of the current instance with updated values.
+  WelcomeModel copyWith() {
+    return WelcomeModel();
+  }
+
+  @override
+  List<Object?> get props => [];
+}
